759e00ed20ef7c008882f74d1e4c52eeb09e40d5
Gage Cane <45441614+gagecane@users.noreply.github.com>
2019-05-24 00:02:36 -0700

    fixed comment line length

--- acne_textgenrnn.py
+++ acne_textgenrnn.py
@@ -5,7 +5,8 @@ import numpy as np
 
 path_to_file = "C:\\Users\\gagec\\Documents\\Code\\Docker\\out.txt"
 
-# You can use train_on_texts with context labels as well by providing a list of context labels of the same size as the texts.
+# You can use train_on_texts with context labels as well by providing
+# a list of context labels of the same size as the texts.
 #
 # In [ ]:
 # texts = ['Never gonna give you up, never gonna let you down',
@@ -16,7 +17,8 @@ path_to_file = "C:\\Users\\gagec\\Documents\\Code\\Docker\\out.txt"
 # context_labels = ['Verse 1', 'Verse 1', 'Verse 2', 'Verse 2']
 #
 # textgen.reset()
-# textgen.train_new_model(texts, context_labels=context_labels, max_length=5, gen_epochs=5, num_epochs=10)
+# textgen.train_new_model(texts, context_labels=context_labels, max_length=5,
+#                         gen_epochs=5, num_epochs=10)
 
 textgen = textgenrnn()
 textgen.train_from_file(path_to_file, num_epochs=100)
